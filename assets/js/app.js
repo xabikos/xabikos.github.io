41a157da36ff85e1858654c22d50c98a9c05553b
Babis Karypidis <xabikos@hotmail.com>
2014-11-08 13:22:19 +0200

    Always show the linked in and feed icons on sidebar

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,13 @@ $( document ).ready(function() {
 	$('.sidebar').css('min-height',$(document).height());
 
 	/* Secondary contact links */
-	var scontacts = $('#contact-list-secondary');
-	var contact_list = $('#contact-list');
+	// var scontacts = $('#contact-list-secondary');
+	// var contact_list = $('#contact-list');
 	
-	scontacts.hide();
+	// scontacts.hide();
 	
-	contact_list.mouseenter(function(){ scontacts.fadeIn(); });
+	// contact_list.mouseenter(function(){ scontacts.fadeIn(); });
 	
-	contact_list.mouseleave(function(){ scontacts.fadeOut(); });
+	// contact_list.mouseleave(function(){ scontacts.fadeOut(); });
 
 });
